b4ba9e0f75232549fb36b204823d2d864300ed5f
Dylan <91469333+Dylan-RDC@users.noreply.github.com>
2022-08-26 17:44:02 +0200

    Removed waste imports

--- src/main/java/com/studentportal/webapp/service/studentService.java
+++ src/main/java/com/studentportal/webapp/service/studentService.java
@@ -3,7 +3,6 @@ package com.studentportal.webapp.service;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import javax.transaction.Transactional;
 
